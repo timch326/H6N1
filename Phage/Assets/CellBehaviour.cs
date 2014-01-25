8fde6b385e9d4014ff0ea848f177873c461233bc
Tim Cheung <paxwell@outlook.com>
2014-01-25 11:56:20 -0800

    Set up TestScene with a dozen of cells placed

---
+++ Phage/Assets/CellBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellBehaviour : MonoBehaviour {
+
+	static public bool isInitialState = true;
+
+	// Use this for initialization
+	void Start () {
+		startGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Destroy overlapping cells 
+	void OnCollisionEnter(Collision coll) {
+		if (coll.gameObject.CompareTag ("Cell")) {
+						Debug.Log ("Collision!", coll.gameObject);
+						Destroy (gameObject);
+				}
+	}
+
+}
---
+++ Phage/Assets/Initialize.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initialize : MonoBehaviour {
+
+	public GameObject Cell;
+
+	// Use this for initialization
+	void Start () {
+		ArrayList clones = new ArrayList();
+		
+		for (int i = 0; i < 10; i++) {
+			GameObject clone;//a clone of the bullet
+			int x = Random.Range(-1,1);
+			int y = Random.Range(-1,1);
+			Vector2 clonePosition = new Vector2 (transform.localPosition.x + x, transform.localPosition.y + y);
+			clone = Instantiate(Cell, clonePosition, transform.rotation) as GameObject;
+			clones.Add(clone);
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
