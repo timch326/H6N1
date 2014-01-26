72f7a0803193110bbd957bd9a004042a10cc8752
David Dang <daviddang.92@gmail.com>
2014-01-26 03:21:17 -0800

    added timer class

---
+++ Phage/Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Awake() {
+		gameObject.renderer.sortingLayerName = "Text";
+
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UpdateTimerText(string t) {
+
+
+		GetComponent<TextMesh>().text = t;
+
+		}
+}
