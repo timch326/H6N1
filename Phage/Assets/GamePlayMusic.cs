ed057836d4f3ff7e2b5bf801ab4a47a63ba9e0fd
Carmen Kong <carmenkakaykong+git@gmail.com>
2014-01-26 12:08:35 -0800

    Added background music to each scene

---
+++ Phage/Assets/GamePlayMusic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayMusic : MonoBehaviour {
+
+	public GameObject currentMusic;
+	public AudioClip levelMusic;
+
+	// Use this for initialization
+	void Awake () {
+		currentMusic = GameObject.Find ("GameMusicSingleton");
+		currentMusic.audio.clip = levelMusic; 
+		currentMusic.audio.Play();
+	}
+}
