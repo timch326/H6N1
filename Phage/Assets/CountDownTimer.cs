16a387c260cf23c789ee737c33cba7902d1ad42b
David Dang <daviddang.92@gmail.com>
2014-01-26 08:44:35 -0800

    added a level count down

---
+++ Phage/Assets/CountDownTimer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDownTimer : MonoBehaviour {
+
+	// Use this for initialization
+	public int level_time = 25;// 25 seconds
+	public float elasped_time = 0;
+
+	void Awake() {
+		gameObject.renderer.sortingLayerName = "Text";
+
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elasped_time += Time.deltaTime;
+		UpdateTimerText (Mathf.RoundToInt(level_time-elasped_time) + " sec");
+		if (elasped_time >= level_time) {
+			Debug.Log("elasped time " + elasped_time);
+			Application.LoadLevel ("test_level_select_ck");		
+		}
+	
+	}
+
+	public void UpdateTimerText(string t) {
+
+
+		GetComponent<TextMesh>().text = t;
+
+		}
+}
